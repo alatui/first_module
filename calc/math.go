a57b81af36559147d6af292e119bfaf8f5ace288
Fernando Cisneros <fernando.saxofone@gmail.com>
2020-05-10 16:20:55 +0100

    Add uses slice of integers

--- calc/math.go
+++ calc/math.go
@@ -1,8 +1,14 @@
 package calc
 
 // Add return sum of two integers
-func Add(i int, j int) int {
-	return i + j
+func Add(numbers ..int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum = sum + num
+	}
+
+	return sum
 }
 
 func substract(i int, j int) int {
